c091853c0693b5188f59d44cbf32620dff1de404
Kejdis Bakalli <66702184+kejdis01@users.noreply.github.com>
2026-07-06 12:56:00 +0200

    Add files via upload

--- ssn-link.js
+++ ssn-link.js
@@ -246,7 +246,7 @@
    * Public API
    * ------------------------------------------------------------ */
   var SSNLink = {
-    VERSION: '8',
+    VERSION: '9',
     session: resolveSession(),
     GEO: { countries: GEO_COUNTRIES, cities: GEO_CITIES },
     matchLocation: matchLocation,
